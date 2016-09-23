e401b199c2a09728da5cc2e2133b9d379210773d
Hernán Patrón Costas <hernanpatroncostas@gmail.com>
2016-09-23 16:01:50 -0300

    Commit inicial

---
+++ APRA-parser/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace APRA_parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Explorar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(367, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Centenario Transfer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Carpeta con archivos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Delay:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 171);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(448, 119);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(396, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Stopped";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Green;
+            this.button3.Location = new System.Drawing.Point(385, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(55, 109);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Tag = "";
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "minutos";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(107, 300);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Autoscroll";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(153, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "URL";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(72, 145);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(110, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Last Push";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            //
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 331);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Centenario Transfer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ APRA-parser/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace APRA_parser
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timerProgresivo = new System.Timers.Timer(25000);
+        System.Timers.Timer timerEnvio = new System.Timers.Timer();
+        String lastPush = "";
+        String file = "config.ini";
+        String head = "[BOF]\n[BOH]\nASCIICode.Separator = 44\nASCIICode.Decimal = 46\nMissingValues = \"\"\nResolution=Min.01\nIntegrationPeriod = Forward\nDateFormat = YYYYMMDD\nTimeFormat = HH:NN\nStation.Name = \"Centenario\"\nStation.Parameters.Count = 12\nStation.Parameters(1).Name =\"NO;Roof;Conc\"\nStation.Parameters(1).Unit =\"ppb\"\nStation.Parameters(1).Position = 1\nStation.Parameters(2).Name =\"NO2;Roof;Conc\"\nStation.Parameters(2).Unit =\"ppb\"\nStation.Parameters(2).Position = 2\nStation.Parameters(3).Name =\"NOx;Roof;Conc\"\nStation.Parameters(3).Unit =\"ppb\"\nStation.Parameters(3).Position = 3\nStation.Parameters(4).Name =\"CO;Roof;Conc\"\nStation.Parameters(4).Unit =\"ppm\"\nStation.Parameters(4).Position = 4\nStation.Parameters(5).Name =\"PM10;Roof;Conc\"\nStation.Parameters(5).Unit =\"ug/m3\"\nStation.Parameters(5).Position = 5\nStation.Parameters(6).Name =\"Wind Direction;6 m;Value\"\nStation.Parameters(6).Unit =\"Degrees\"\nStation.Parameters(6).Position = 6\nStation.Parameters(7).Name =\"Wind Speed;Mast 6m;Value\"\nStation.Parameters(7).Unit =\"m/s\"\nStation.Parameters(7).Position = 7\nStation.Parameters(8).Name =\"Temperature;Mast 2m;Value\"\nStation.Parameters(8).Unit =\"Celsius\"\nStation.Parameters(8).Position = 8\nStation.Parameters(9).Name =\"Rel Humidity;Mast 2m;Value\"\nStation.Parameters(9).Unit =\"%\"\nStation.Parameters(9).Position = 9\nStation.Parameters(10).Name =\"Atm Pressure;Roof;Value\"\nStation.Parameters(10).Unit =\"hPa\"\nStation.Parameters(10).Position = 10\nStation.Parameters(11).Name =\"Global Radiation;Roof;Value\"\nStation.Parameters(11).Unit =\"W/m2\"\nStation.Parameters(11).Position = 11\nStation.Parameters(12).Name =\"Rain;Roof;Value\"\nStation.Parameters(12).Unit =\"mm\"\nStation.Parameters(12).Position = 12\n[EOH]\n[BOD]\n";
+        String data = "";
+        bool estado = false;
+        String carpeta = "";
+        bool scroll = true;
+        String url = "";
+        String configs = "";
+        DateTime last;
+        String info;
+        int global = 0;
+        String[] sensores = { "atmpressure", "carbonoxide", "globalradiation", "mononitrogenoxide", "nitricdioxide", "nitricoxide", "particulatematter", "rain", "relativehumidity", "temperature", "winddirection", "windspeed" };
+
+        private void pushData(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            global++;
+            var listaArch = Directory.GetFiles(carpeta);
+            foreach(String archivo in listaArch)
+            {
+                String archivoFin = archivo.Substring(carpeta.Length+1);
+                if (archivoFin == String.Format("PromedioMin{0}.Calc", last.Month.ToString()))
+                {
+                    addLog(String.Format("Archivo {0} existente", archivoFin));
+                    String line;
+                    StreamReader reader = new StreamReader(File.Open(archivo,
+                           FileMode.Open,
+                           FileAccess.Read,
+                           FileShare.ReadWrite));
+                    int cont = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lastPush = last.ToString("dd/MM/yy,HH:mm");
+                        if (line.IndexOf(lastPush) != -1)
+                        {
+                            cont++;
+                            DateTime lastPushDate = DateTime.Parse(lastPush);
+                            info = lastPushDate.ToString("yyyyMMdd,HH:mm") + line.Substring(lastPush.Length)+"\n";
+                            addLog(lastPush + ": Nueva información encontrada: ");
+                            addLog(info);
+                            data += info;
+                            estado = true;
+                            last = last.AddMinutes(1.00);
+                            global = 0;
+                        }
+                    }
+                    if (cont == 0 && global>3)
+                    {
+                        if (last < DateTime.Now)
+                            last = last.AddMinutes(1.00);
+                        else
+                            last = DateTime.Now;
+                    }
+                    reader.Close();
+                }
+            }
+        }
+        private void addLog(String text)
+        {
+            if (InvokeRequired)
+            {
+                richTextBox1.Invoke((Action) delegate
+                {
+                    richTextBox1.AppendText(text + "\n");
+                    if(scroll)
+                        richTextBox1.ScrollToCaret();
+                });
+            }
+            else
+            {
+                richTextBox1.AppendText(text + "\n");
+                if (scroll)
+                    richTextBox1.ScrollToCaret();
+            }
+        }
+        private int sendData(String datos)
+        {
+            try
+            {
+                var baseUri = new Uri(url);
+                WebClient client = new WebClient();
+                client.Headers.Add("Content-Type", "text/plain");
+                datos = head + datos + "[EOD]\n[EOF]";
+                addLog(datos);
+                var response = client.UploadString(baseUri, datos);
+                addLog("Mandando a la API");
+                return 202;
+            }
+            catch (WebException ex)
+            {
+                return (int)((HttpWebResponse)ex.Response).StatusCode;
+            }
+        }
+
+        private void send(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (estado && data != "")
+            {
+                int code = sendData(data);
+                if (code == 200 || code == 202)
+                {
+                    addLog("Información enviada a la API");
+                    estado = false;
+                    data = "";
+                }
+                else
+                {
+                    addLog("Error pusheando a la API: " + code.ToString());
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(file))
+            {
+                String contenido = File.ReadAllText(file);
+                String[] configurations = contenido.Split('\n');
+                comboBox1.Text = configurations[0];
+                textBox1.Text = configurations[1];
+            }
+            else
+            {
+                textBox1.Text = "http://bapocbulkserver.azurewebsites.net/api1/otfs/centenario";
+            }
+            timerEnvio.Elapsed += send;
+            timerProgresivo.Elapsed += pushData;
+            addLog("Programa inicializado");
+            addLog("El servicio está actualmente detenido");
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dial = new FolderBrowserDialog();
+            DialogResult result = dial.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                comboBox1.Text = dial.SelectedPath;
+            }
+            addLog("Carpeta Seleccionada: " + comboBox1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (label5.Text == "Stopped" && Directory.Exists(comboBox1.Text))
+            {
+                carpeta = comboBox1.Text;
+                label5.Text = "Running";
+                label5.ForeColor = Color.Green;
+                button3.Text = "STOP";
+                timerProgresivo.Start();
+                timerEnvio.Interval = (int)numericUpDown1.Value * 60000;
+                timerEnvio.Start();
+                numericUpDown1.Enabled = false;
+                comboBox1.Enabled = false;
+                button1.Enabled = false;
+                dateTimePicker1.Enabled = false;
+                textBox1.Enabled = false;
+                configs += carpeta + "\n";
+                configs += textBox1.Text + "\n";
+                last = dateTimePicker1.Value;
+                button3.ForeColor = Color.Red;
+                addLog("Servicio comenzado...");
+                File.WriteAllText(file, configs);
+            }
+            else if (label5.Text == "Stopped" && !Directory.Exists(comboBox1.Text))
+            {
+                MessageBox.Show("Carpeta no seleccionada o Inexistente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                addLog("Error en la selección de carpeta");
+            }
+            else
+            {
+                numericUpDown1.Enabled = true;
+                comboBox1.Enabled = true;
+                textBox1.Enabled = true;
+                button1.Enabled = true;
+                dateTimePicker1.Enabled = true;
+                label5.Text = "Stopped";
+                timerProgresivo.Stop();
+                label5.ForeColor = Color.Red;
+                timerEnvio.Stop();
+                button3.Text = "START";
+                button3.ForeColor = Color.Green;
+                addLog("Servicio terminado...");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                scroll = true;
+            }
+            else
+            {
+                scroll = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            url = textBox1.Text;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
